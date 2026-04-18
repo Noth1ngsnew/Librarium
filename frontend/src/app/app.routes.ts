945716c3755865262d7e772ec0b4fcdac5a904e5
Noth1ngsnew <skabekovdanial@gmail.com>
2026-04-18 15:22:14 +0500

    angular project setup with routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { BookList } from './components/book-list/book-list';
+import { BookDetail } from './components/book-detail/book-detail';
+import { MyList } from './components/my-list/my-list';
+import { Badges } from './components/badges/badges';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'books', component: BookList },
+  { path: 'books/:id', component: BookDetail },
+  { path: 'my-list', component: MyList },
+  { path: 'badges', component: Badges },
+];
