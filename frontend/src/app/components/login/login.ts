5b8a1be3b0409a9a2b3dde665da146418f527bd7
kuwloveu <r_khassanov@kbtu.kz>
2026-04-19 17:32:01 +0500

    fixed component login page

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
+  isRegister = signal(false);
+  error = signal('');
+
+  formData = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  toggleMode() {
+    this.isRegister.set(!this.isRegister());
+    this.error.set('');
+  }
+
+  submit() {
+    if (this.isRegister()) {
+      this.auth.register(this.formData).subscribe({
+        next: (res: any) => {
+          this.auth.saveTokens(res.access, res.refresh);
+          this.router.navigate(['/books']);
+        },
+        error: (err) => {
+          this.error.set(err.error?.username?.[0] || 'Registration failed.');
+        }
+      });
+    } else {
+      this.auth.login(this.formData).subscribe({
+        next: (res: any) => {
+          this.auth.saveTokens(res.access, res.refresh);
+          this.router.navigate(['/books']);
+        },
+        error: () => {
+          this.error.set('Invalid username or password.');
+        }
+      });
+    }
+  }
 }
